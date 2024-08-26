6777bc2a406368d2047cbdf2ea9315fadc039c49
Md Fuad Amir <mdfuadamir@gmail.com>
2024-08-26 16:23:11 +0600

    added contact us section

--- src/Component/Community/Community.jsx
+++ src/Component/Community/Community.jsx
@@ -4,7 +4,7 @@ const Community = () => {
     return (
         <div className="max-w-6xl mx-auto px-4 py-6 md:py-20">
             <div className="flex flex-col-reverse md:flex-row items-center justify-between gap-6">
-                <div className="w-full md:w-1/2 space-y-6">
+                <div className="w-full md:w-1/2 space-y-6 text-center">
                 <h1 className="text-4xl md:text-5xl font-bold font-serif ">Join Our Community to Start your Journey</h1>
                 <p className="text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, repudiandae possimus cum laudantium voluptatibus porro doloribus odio at id, corrupti ut et labore, amet quisquam laborum excepturi! Consectetur, deleniti amet id natus nisi dolorum, ipsa illum dicta animi consequatur rerum sapiente quae, maiores libero eum. Dolores iusto cupiditate accusantium officia.</p>
                 <button className="px-6 py-2 bg-yellow-400 font-semibold text-white rounded-xl flex items-center gap-2 shadow-[0px_10px_10px_-7px_#e4e810] hover:bg-[#73b1a6] duration-300">Join Now</button>
--- src/Component/ContactUs/ContactUs.jsx
+++ src/Component/ContactUs/ContactUs.jsx
@@ -1,8 +1,33 @@
-
+// #73b1a6
 
 const ContactUs = () => {
     return (
-        <div>
+        <div className="py-6 md:py-20 px-4 max-w-6xl mx-auto">
+            <div className="max-w-2xl p-4 mx-auto bg-gray-200/40 rounded-2xl">
+                <h1 className="text-3xl font-bold font-serif text-center"><span className="text-[#73b1a6]">Contact</span>  us</h1>
+                <div>
+                <from>
+                    <div className="space-y-4 py-12">
+                    <input
+                    type="text"
+                    placeholder="Your Name"
+                    className="input input-bordered input-success w-full"/>
+                    <input
+                    type="number"
+                    placeholder="Your Phone Number"
+                    className="input input-bordered input-success w-full"/>
+                    <input
+                    type="email"
+                    placeholder="Your Email"
+                    className="input input-bordered input-success w-full"/>
+                    <textarea className="textarea textarea-success w-full" placeholder="Your Massage"></textarea>
+                    <div className="flex justify-center items-center">
+                    <button className="px-6 py-2 bg-yellow-400 font-semibold text-white rounded-xl flex items-center gap-2 shadow-[0px_10px_10px_-7px_#e4e810] hover:bg-[#73b1a6] duration-300">Contact Us</button>
+                    </div>
+                    </div>
+                </from>
+                </div>
+            </div>
             
         </div>
     );
